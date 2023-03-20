81fa1dff2915484daa416225f6c597242c3785ce
Ajay giri <ajaygiri9099@gmail.com>
2023-03-20 17:33:53 -0400

    added js file

---
+++ script.js
@@ -0,0 +1,68 @@
+//output
+
+var mystring = 'Hello, world!'
+console.log(mystring)
+
+let myname= 'My name is Ajay Giri'
+let learning= 'I am Learning JavaScript via Full Stack Bootcamp of DiaaTech.'
+let iwill= 'I will become the best developer by hard work.'
+let commitment= 'I am making commitment to do code with consistency.'
+
+console.log(myname)
+console.log(learning)
+console.log(iwill)
+console.log(commitment)
+
+//variables 1-
+let name = 'Ajay Giri'
+let age='25'
+
+console.log(name,age)
+
+//variable 2-
+
+let Name= 'Ajay Giri'
+let course= 'JavaScript via Full Stack Bootcamp of DiaaTech.'
+let willbe= 'the best developer by hard work.'
+let make= 'I am making commitment to do code with consistency.'
+
+let myinformation = `My name is ${'Mr. '+Name}, i am learning ${course},i will become ${willbe}, ${make} `
+console.log(myinformation)
+
+//variable 3-
+let numberone = 10;
+let numbertwo = 15;
+
+let sum = numberone + numbertwo
+console.log(sum)
+console.log('minus:',numberone-numbertwo)
+console.log('multipy:',numberone*numbertwo)
+console.log('division',numberone/numbertwo)
+
+//BMI
+
+let markmass = 55;
+let markheight = 1.67;
+
+console.log(markmass / (markheight * markheight))
+
+let johnmass = 90;
+let johnheight = 1.8;
+
+console.log(johnmass / (johnheight * johnheight))
+
+
+let bmi = 27.77
+
+if(bmi  >25){
+    console.log('john has higher bmi')
+}
+else{
+    console.log('mark bmi is less than john bmi')
+}
+
+
+
+
+
+
